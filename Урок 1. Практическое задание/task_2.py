1e7a8a528eea25cc1bc2db499084200bb35569cd
Dmitry Ageev <ageev.dmitry.s@gmail.com>
2022-09-05 20:47:49 +0300

    add tasks 1,2,3 for lesson 1

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,28 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+
+# Линейная O(n)
+def search_linear(lst: list):   # O(n)
+    smallest = some_list[0]     # O(1)
+    for i in some_list:         # O(n)
+        if i < smallest:        # O(1)
+            smallest = i        # O(1)
+    return smallest             # O(1)
+
+
+# Квадратичная O(n**2)
+def search_square(lst: list):
+    for i in some_list:         # O(n)
+        stop = True             # O(1)
+        for el in some_list:    # O(n)
+            if i > el:          # O(1)
+                stop = False    # O(1)
+        if stop:                # O(1)
+            return i            # O(1)
+
+
+some_list = [1, 2, 3, 100, 42, 13, 228, 0, 1337, 5]
+print(search_linear(some_list))
+print(search_square(some_list))
--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -17,3 +17,27 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+company_data = {'company_name_1': 250000,
+                'company_name_2': 1337000,
+                'company_name_3': 111000,
+                'company_name_4': 9000,
+                'company_name_5': 1234,
+                }
+
+# Вариант 1. Cложность: O(n log n)
+max_comp = sorted(company_data.items(), key=lambda para: para[1], reverse=True)[:3]   # O(n log n)
+for company in max_comp:                                                              # O(n)
+    print(f'{company[0]} : {company[1]}')
+
+# Вариант 2. Cложность: O(n)
+top_company = {}                                                                           # O(1)
+temp_dict = dict(company_data)                                                             # O(1)
+for i in range(3):                                                                         # O(n)
+    top = max(temp_dict.items(), key=lambda val: val[1])                                   # O(n)
+    top_company[top[0]] = top[1]                                                           # O(1)
+    del temp_dict[top[0]]                                                                  # O(1)
+for company, profit in top_company.items():                                                # O(n)
+    print(f'{company} : {profit}')
+
+# Второй вариант оптимальнее, т.к. сложность линейная и соответственно с ростом данным,
+# время выполнения будет меньше, чем у линейно-логарифмической модели.
